6b016ff75aa311e5094481c729cb357f43f27b41
Tiago Paulino <tspaulino@gmail.com>
2012-03-13 00:00:10 -0300

    Starting project

---
+++ config/initializers/mongo.rb
@@ -0,0 +1,3 @@
+logger = Logger.new("log/mongodb-#{Rails.env}.log")
+MongoMapper.connection = Mongo::Connection.new('127.0.0.1', 27017, :logger => logger)
+MongoMapper.database = "YourApp-#{Rails.env}"
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blacklist::Application.config.secret_token = 'c62d6b3f5c012df66ddb17b2d572c2c9057112fe754e40a663689b0f1e492c20bb7579feb244f09e198dcb016b8b5f3b257fa766f58e720029f6c5b9b50d9284'
